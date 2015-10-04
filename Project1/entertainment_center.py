5891ff8233b2a9751f5083f2dff705513dfc9877
personjc <personjc@gmail.com>
2015-10-04 19:55:48 -0400

    First check in of Project1

---
+++ Project1/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "poster",
+                        "trailer")
+
+print toy_story.storyline

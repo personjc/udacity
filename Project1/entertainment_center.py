3a471415d85489cbbe1d1e0708b00b6fea2a5636
personjc <personjc@gmail.com>
2015-10-06 22:48:15 -0400

    Changed a movie, learning staging/commits

--- Project1/entertainment_center.py
+++ Project1/entertainment_center.py
@@ -1,17 +1,17 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "poster",
-                        "trailer")
+days_of_summer = media.Movie("500 Days of Summer",
+                        "This is a story of boy meets girl, but this is not a love story.",
+                        "https://upload.wikimedia.org/wikipedia/en/d/d1/Five_hundred_days_of_summer.jpg",
+                        "https://www.youtube.com/watch?v=PsD0NpFSADM")
 
 good_men = media.Movie( "A Few Good Men",
                         "Two soldiers are put on trial over the death of a fellow solider",
                         "https://upload.wikimedia.org/wikipedia/en/4/45/A_Few_Good_Men_poster.jpg",
                         "https://www.youtube.com/watch?v=ePo91pMcu94")
 
-movies= [toy_story, good_men]
+movies= [days_of_summer, good_men]
 
 fresh_tomatoes.create_movie_tiles_content(movies)
 fresh_tomatoes.open_movies_page(movies)

9c5ed0db309a0d2864ef419ba8288e0e0e7c4171
personjc <personjc@gmail.com>
2015-10-18 23:59:21 -0400

    Adding comments

--- Project1/media.py
+++ Project1/media.py
@@ -1,7 +1,17 @@
 import webbrowser
 
 class Movie():
+    """ Movie object for the entertainment center
+
+    Attributes:
+        movie_title: title of the selected movie
+        movie_storyline: short synopsis of movie plot
+        poster_image: picture of the official movie poster
+        trailer_youtube: url of the movie trailer
+        year: release year for the movie
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, year):
+        """initializes movie object taking in all required attributes"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -9,4 +19,5 @@ class Movie():
         self.year = year
 
     def show_trailer(self):
+        """Opens movie trailer in video viewer"""
         webbrowser.open(self.trailer_youtube_url)

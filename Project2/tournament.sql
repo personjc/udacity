631dc1728d2c90d1987ab6d17b33fa79c402533a
personjc <personjc@gmail.com>
2016-02-14 16:25:13 -0500

    adding initial create statements

--- Project2/tournament.sql
+++ Project2/tournament.sql
@@ -6,4 +6,27 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+--Create players
+CREATE TABLE players (
+id serial primary key,
+name text);
+
+--Create matches
+CREATE TABLE matches (
+id serial primary key,
+player_one text foreign key,
+player_two text foreign key,
+result text);
+
+--create records
+CREATE TABLE records (
+player_id serial primary key,
+record text);
+
+CREATE VIEW player_card (
+SELECT p.id, p.name, r.record 
+FROM players p
+JOIN records r on
+p.id = r.player_id);
+
 
